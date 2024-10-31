de26036a2608da24f8e0b12f6bbf2d9fc516b3ed
= <miguelcastrejongal@gmail.com>
2024-10-30 21:31:04 -0600

    Create a third app called littlelemonapi for  unit testing

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
+    'LittleLemonAPI',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
